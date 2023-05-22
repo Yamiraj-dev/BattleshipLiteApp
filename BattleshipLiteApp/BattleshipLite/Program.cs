3fd49a14a7cef6e2243f013a72a7eb89291ac7cf
Yamiraj <yamiraj.dev@gmail.com>
2023-05-23 03:49:04 +0800

    Created class models and game logic class with some methods.

---
+++ BattleshipLiteApp/BattleshipLite/Program.cs
@@ -0,0 +1,44 @@
+﻿using BattleshipLiteLibrary;
+using BattleshipLiteLibrary.Models;
+
+namespace BattleshipLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            Console.ReadLine();
+        }
+
+        public static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("created by Yamiraj");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer()
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            // Ask player name
+            output.PlayersName = AskPlayerName();
+            // Load up shot grid
+            GameLogic.GridInitialise(output);
+
+            // Ask player for 5 ship placements
+            // Clear
+
+        }
+
+        private static string AskPlayerName()
+        {
+            Console.WriteLine("What is your player name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+    }
+}
